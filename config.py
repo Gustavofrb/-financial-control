3ccb5d742070413db4c646eccb284efe74e65a8c
Gustavo Felipe <gustavo.barbosa@centralit.com.br>
2025-06-22 13:39:32 -0300

    render

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///contas.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
+    SECRET_KEY ='6cb60152e9a082d6c92251e6d56a64b762f1343d0247a34d386c6e61ea100bd9'
 
-    # Cookies cross-domain para produção
-    SESSION_COOKIE_SAMESITE = "None"
-    SESSION_COOKIE_SECURE = os.getenv('FLASK_ENV') == "production"
     SESSION_TYPE = "filesystem"
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True
